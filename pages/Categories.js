743c7d923ea7894c859797a0cbf2d574166e234d
Goriock <d.dib@gmail.com>
2023-09-06 17:47:21 +0100

    Deploy

--- pages/Categories.js
+++ pages/Categories.js
@@ -12,17 +12,19 @@ export default function CategoriesPage({MainCategories,hasParent}) {
                     <div className='w-4/5 p-2' key={MainCategory._id}>
                       <h2 className='text-2xl'>  {MainCategory.name}</h2>
                         <div className='flex flex-wrap gap-2 p-2'>
-                            {hasParent?.length>0 && hasParent.map(subcategory=>{
-                                if (subcategory.parent==MainCategory._id)
-                                {return(
-                                    <Link href={'/Categories/'+subcategory._id} key={subcategory._id}>
-                                    <div className='bg-slate-400 w-48 h-40 rounded-md flex flex-col item-center  ' key={subcategory}>
-                                        <h2 className='text-center text-lg'>{subcategory.name}</h2>
-                                    <img src={subcategory?.imageUrl} className='h-32 mt-1 w-full'alt='category image'/>
-                                    </div>
-                                    </Link>
-                                )}
-                            })}
+                        {hasParent?.length > 0 &&
+  hasParent.map(subcategory => {
+    if (subcategory.parent === MainCategory._id) {
+      return (
+        <Link href={'/Categories/' + subcategory._id} key={subcategory._id}>
+          <div className='bg-slate-400 w-48 h-40 rounded-md flex flex-col item-center'>
+            <h2 className='text-center text-lg'>{subcategory.name}</h2>
+            <img src={subcategory?.imageUrl} className='h-32 mt-1 w-full' alt='category image' />
+          </div>
+        </Link>
+      );
+    }
+  })}
                         </div>
                         </div>
                 )
