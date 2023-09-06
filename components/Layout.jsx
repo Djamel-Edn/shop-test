3a414b0e7427c99f83fe4c8c6f2be6d7a3e1c814
Goriock <d.dib@gmail.com>
2023-09-06 21:47:09 +0100

    deploy

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './footer'
+import React from 'react';
+import Header from './Header';
+import Footer from './footer';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
+  const router = useRouter();
+  
+  const Title = router.asPath.substring(1,16)
+
   return (
     <div className='flex flex-col min-h-screen'>
-        <Header/>
-    <div className=''>{children}</div>
-    <Footer/>
+      <Head>
+        <title>{Title}</title>
+        <link rel="shortcut icon" href="" type="image/x-icon" />
+      </Head>
+      <Header />
+      <div className=''>{children}</div>
+      <Footer />
     </div>
-
-
-    
-  )
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,6 @@ export default function App({ Component, pageProps }) {
   <div className={roboto.className}>
         <CartContextProvider>
       <Layout>
-
     <Component {...pageProps} />
       </Layout>
         </CartContextProvider>
